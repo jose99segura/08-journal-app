5c4f086ddef1b252565139040eb04d319afe606f
jose99segura <jose99segura@gmail.com>
2024-10-21 22:28:03 +0200

    first commit

---
+++ src/journal/routes/JournalRoutes.jsx
@@ -0,0 +1,14 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { JournalPage } from "../pages/JournalPage"
+
+
+
+export const JournalRoutes = () => {
+  return (
+    <Routes>
+        <Route path="/journal" element={<JournalPage />} />
+
+        <Route path="/*" element={<Navigate to="/" />} />
+    </Routes>
+  )
+}
